7e0ddc4a23ca766afed11afa7ed2c8c7fc403af8
220101ft <220101ft@yse-c.net>
2023-12-13 11:21:18 +0900

    update

--- server.js
+++ server.js
@@ -16,6 +16,26 @@ dotenv.config();
 const host = process.env.HOST;
 const port = process.env.PORT;
 
+// Socket.io モジュール読み込み
+const { Server } = require("socket.io");
+// Socket.io で通信
+const io = new Server();
+
+// connection event
+io.on("connection", (socket) => {
+    console.log("connected!!!");
+
+    //chatメッセージの受信
+    socket.on("chat_message", (data) => {
+        console.log(socket.id);
+        console.log(data);
+        //送信ユーザのSocketIDを追加
+        data.socket_id = socket.id;
+        //接続しているユーザにメッセージを送信
+        io.emit("chat_message", data);
+    });
+});
+
 // サーバ待機
 server.listen(port, host, () => {
     console.log(`listening on http://${host}:${port}`);
