f98e50a39e68c0aef6acc6b7117cc001db0800f2
220101ft <220101ft@yse-c.net>
2023-12-13 12:01:29 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,9 @@ const socket = io.connect(URL);
 
 socket.on("chat_message", (data) => {
     console.log(data);
+    var p = document.createElement("p");
+    p.innerText = data.socketID + ": " + data.message;
+    document.getElementById("chat-list").prepend(p);
 });
 
 function sendMessage() {
